8140ff54e4671c75cb88de2c6966c2064011fbcb
5ajaki <help>
2023-06-24 12:42:29 -0400

    Added query use Fetch

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,45 @@
 import Head from 'next/head'
 
+import { useFetch } from '@/hooks/useFetch'
+
+const endpoint =
+  'https://api.studio.thegraph.com/query/48901/loo-hack-fo-ens-token/v0.0.5'
+
+const query = `
+  {
+    delegateChangeds {
+      id
+      delegator
+      fromDelegate
+      toDelegate
+      transactionHash
+    }
+  }
+`
+
+type GraphResponse = {
+  data: {
+    delegateChangeds: {
+      id: string
+      delegator: string
+      fromDelegate: string
+      toDelegate: string
+      transactionHash: string
+    }[]
+  }
+}
+
 export default function Home() {
+  const { data, error } = useFetch<GraphResponse>(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+
+  const delegateChangeds = data?.data?.delegateChangeds
+
   return (
     <>
       <Head>
@@ -14,6 +53,13 @@ export default function Home() {
 
       <main>
         <p>hello world</p>
+
+        {delegateChangeds && (
+          <>
+            <p>my data loaded yay!</p>
+            {JSON.stringify(delegateChangeds[0])}
+          </>
+        )}
       </main>
     </>
   )
